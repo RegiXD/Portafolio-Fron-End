5ffafc7df031ee7028c430dca52aeeecf6057a1e
RegiXD <reitanomariaregina@gmail.com>
2022-12-14 19:58:42 -0300

    El enter anda cuando tiene que andar :)

--- Actions.js
+++ Actions.js
@@ -13,8 +13,32 @@ const ediciones=document.getElementsByClassName('editar');
 const cerrar=document.getElementsByClassName('cerrar');
 const login_logout=document.getElementById('Login');
 const modal = new bootstrap.Modal(document.getElementById('ModalLogin'));
+const input= document.getElementsByClassName('input');
+const formulario=document.getElementById('form');
+const divInvalidU = document.createElement("div");
+const divInvalidC = document.createElement("div");
+const CI =document.createTextNode("contraseña invalida")
+const UI =document.createTextNode("uasuario invalido")
+divInvalidU.setAttribute("class", "invalid-feedback");
+divInvalidC.setAttribute("class", "invalid-feedback");
+divInvalidU.appendChild(UI);
+divInvalidC.appendChild(CI);
 
 boton.addEventListener('click', (e) => {
+    logearse();
+})
+
+document.addEventListener('keydown', (e) => {
+    console.log(modal._isShown);
+    if(modal._isShown){
+        if(e.code==='Enter'){
+            logearse();
+        }
+    }
+    else e.preventDefault();
+})
+
+function logearse(e){
     if(usuario.value==verificacion.usuario && contrasenia.value==verificacion.contrasenia){
         usuario.value="";
         contrasenia.value="";
@@ -28,8 +52,18 @@ boton.addEventListener('click', (e) => {
         for(let i=0; i<cerrar.length; i++)
             cerrar[i].setAttribute("class", "cerrar btn btn-success bi-x-lg rounded-circle");
     }
-    else console.log("INAVÁLIDO");
-})
+    else{
+        if(contrasenia.value!==verificacion.contrasenia){
+            input[1].setAttribute("class", "input form-control is-invalid");
+        }
+        else input[1].setAttribute("class", "input form-control is-valid");
+
+        if(usuario.value!==verificacion.usuario){
+            input[0].setAttribute("class", "input form-control is-invalid");
+        }
+        else input[0].setAttribute("class", "input form-control is-valid");
+    } 
+}
 
 login_logout.addEventListener('click', (e) => {
     if(login_logout.getAttribute("class") == "Salir btn btn-danger btn-sm bi-box-arrow-left"){
@@ -42,4 +76,8 @@ login_logout.addEventListener('click', (e) => {
         for(let i=0; i<cerrar.length; i++)
             cerrar[i].setAttribute("class", "cerrar btn btn-success bi-x-lg rounded-circle d-none");
     }
+})
+
+ediciones[0].addEventListener("click", (e) => {
+    
 })
